c20c0a35b4f136a40624cbaec2de8e201159dfd3
Raffael <github@raffaeljesche.de>
2019-07-13 00:16:57 +0200

    Revert "fixed wrong variable for gallery template"
    This reverts commit e10a8ea941b96b4ab12592d68225436cf3de95ea.

--- modules/Multiplane/themes/rljbase/index.php
+++ modules/Multiplane/themes/rljbase/index.php
@@ -18,7 +18,7 @@ $height = mp()->get('lexy/headerimage/height', 200) . 'px';
             {{ $page['content'] }}
 
             @if(!empty($page['gallery']))
-                @render('views:partials/gallery.php', ['gallery' => $page['gallery']])
+                @render('views:partials/gallery.php', compact('page'))
             @endif
 
             @if(!empty($page['video']))
