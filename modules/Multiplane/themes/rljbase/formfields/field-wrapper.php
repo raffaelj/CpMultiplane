801614fb9e672f8c845cb39bcdb55824995368fb
Raffael <github@raffaeljesche.de>
2019-08-07 21:33:15 +0200

    added `@lang` to text before/after linked page of form item
    and removed line breaks to prevent spaces after linked page

--- modules/Multiplane/themes/rljbase/formfields/field-wrapper.php
+++ modules/Multiplane/themes/rljbase/formfields/field-wrapper.php
@@ -1,4 +1,3 @@
-<?php $slugName = cockpit('multiplane')->slugName; ?>
 
 <div class="width-{{ $field['width'] ?? '1-1' }}">
     {{ $content_for_layout }}
@@ -10,13 +9,8 @@
 @endif
 @if(!empty($field['link']))
     <p class="message info link">
-  @if(!empty($field['link']['text_before']))
-      {{ $field['link']['text_before'] }}
-  @endif
-        <a href="@base($field['link'][$slugName])">{{ $field['link']['title'] ?? 'link' }}</a>
-  @if(!empty($field['link']['text_after']))
-      {{ $field['link']['text_after'] }}
-  @endif
+    @if(!empty($field['link']['text_before']))@lang($field['link']['text_before'])@endif
+    <a href="@base($field['link'][mp()->slugName])">{{ $field['link']['title'] ?? 'link' }}</a>@if(!empty($field['link']['text_after']))@lang($field['link']['text_after'])@endif
     </p>
 @endif
 </div>
