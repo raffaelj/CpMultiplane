4a969d2a5d79a84c34b90da3b86a1c6b182a15d6
Raffael <github@raffaeljesche.de>
2019-09-12 15:52:56 +0200

    fixed wrong plural for 0 in search entries

--- modules/Multiplane/themes/rljbase/search.php
+++ modules/Multiplane/themes/rljbase/search.php
@@ -2,7 +2,7 @@
 <main>
     @render('views:partials/search.php')
     @if (isset($error))<p>@lang($error)</p>@endif
-    <p>{{ count($list) }} @lang($count <= 1 ? 'result' : 'results')</p>
+    <p>{{ count($list) }} @lang($count == 1 ? 'result' : 'results')</p>
     @foreach($list as $l)
     <div class="search-entries">
         <h3><a href="{{ $l['url'] }}">{{ $l['title'] }}</a></h3>
