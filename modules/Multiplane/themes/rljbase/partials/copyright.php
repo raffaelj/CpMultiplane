c77e48285615d362156e02d583144d89cd5af531
Raffael <github@raffaeljesche.de>
2019-09-12 15:52:01 +0200

    added class to copyright partial

--- modules/Multiplane/themes/rljbase/partials/copyright.php
+++ modules/Multiplane/themes/rljbase/partials/copyright.php
@@ -1,1 +1,1 @@
-<p>@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy; @lang('since') 2019 <a href="https://www.raffael.one">Raffael Jesche</a></p>
+<p class="copyright">@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy; @lang('since') 2019 <a href="https://www.raffael.one">Raffael Jesche</a></p>
