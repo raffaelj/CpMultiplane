e3a069ef4c1a9a4106f482edc8d35d09ff7dd3dd
Raffael <github@raffaeljesche.de>
2019-09-17 00:33:47 +0200

    added pretty search icon

--- modules/Multiplane/themes/rljbase/partials/search.php
+++ modules/Multiplane/themes/rljbase/partials/search.php
@@ -2,6 +2,6 @@
 <div class="search">
     <form action="@base('/')">
         <input name="search" type="text" value="{{ $app->escape($app->param('search', '')) }}" minlength="{{ mp()->searchMinLength }}" />
-        <button type="submit">@lang('Search')</button>
+        <button type="submit"><i class="icon-search"></i>@lang('Search')</button>
     </form>
 </div>
