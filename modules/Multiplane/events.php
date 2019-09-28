c5a84c02efe517563ce54fdf6acb1fd5c51f45ef
Raffael <github@raffaeljesche.de>
2019-09-28 21:41:43 +0200

    fixed route detection in sitemap for sub pages

--- modules/Multiplane/events.php
+++ modules/Multiplane/events.php
@@ -196,6 +196,8 @@ $this->on('multiplane.sitemap', function(&$xml) {
     $defaultLang    = mp()->defaultLang;
     $slugName       = mp()->slugName;
     $languages      = [];
+    $parentPage     = null;
+    $route          = '';
 
     $collections = mp()->sitemap ?? [mp()->pages, mp()->posts];
 
@@ -205,6 +207,7 @@ $this->on('multiplane.sitemap', function(&$xml) {
         ],
         'fields' => [
             $slugName => true,
+            '_modified' => true,
         ],
     ];
 
@@ -225,39 +228,75 @@ $this->on('multiplane.sitemap', function(&$xml) {
 
         foreach($this->module('collections')->find($collection, $options) as $page) {
 
+            if ($collection != mp()->pages) {
+
+                $filter = [
+                    'published' => true,
+                    'subpagemodule.active' => true,
+                ];
+                $projection = [
+                    '_id' => false,
+                    'subpagemodule' => true,
+                ];
+
+                $parentPage = $this->module('collections')->findOne(mp()->pages, $filter, $projection, false, null);
+
+            }
+
             if (!$isMultilingual) {
-              $xml->startElement('url');
-                $xml->startElement('loc');
-                $xml->text($siteUrl . '/' .  $page[mp()->slugName]);
+
+                $route = '';
+                if ($collection != mp()->pages
+                    && !empty($parentPage['subpagemodule']['route'])) {
+                    $route = '/' . $parentPage['subpagemodule']['route'];
+                }
+
+                $xml->startElement('url');
+                  $xml->startElement('loc');
+                  $xml->text($siteUrl . $route . '/' . $page[mp()->slugName]);
+                  $xml->endElement();
+
+                  $xml->startElement('lastmod');
+                  $xml->text(date('c', ($page['_modified']) ?? $page['_created']));
+                  $xml->endElement();
                 $xml->endElement();
 
-                $xml->startElement('lastmod');
-                $xml->text(date('c', $page['_modified']));
-                $xml->endElement();
-              $xml->endElement();
             }
 
             else {
                 foreach ($languages as $lang) {
 
                     $suffix = $lang == $defaultLang ? '' : '_' . $lang;
+                    $route = '';
+
+                    if ($collection != mp()->pages
+                        && !empty($parentPage['subpagemodule']['route'.$suffix])) {
+                        $route = '/' . $parentPage['subpagemodule']['route'.$suffix];
+                    }
 
                     $xml->startElement('url');
 
                       $xml->startElement('loc');
-                      $xml->text($siteUrl . "/$lang/" . $page[$slugName.$suffix]);
+                      $xml->text($siteUrl . "/$lang" . $route . '/' . $page[$slugName.$suffix]);
                       $xml->endElement();
 
                       foreach ($languages as $l) {
 
                           if ($l == $lang) continue;
+                          $suffix = $l == $defaultLang ? '' : '_' . $l;
+                          $route = '';
+
+                          if ($collection != mp()->pages
+                              && !empty($parentPage['subpagemodule']['route'.$suffix])) {
+                              $route = '/' . $parentPage['subpagemodule']['route'.$suffix];
+                          }
 
                           $suffix = $l == $defaultLang ? '' : '_' . $l;
 
                           $xml->startElement('xhtml:link');
                           $xml->writeAttribute('rel', 'alternate');
                           $xml->writeAttribute('hreflang', $l);
-                          $xml->writeAttribute('href', $siteUrl . "/$l/" . $page[$slugName . $suffix]);
+                          $xml->writeAttribute('href', $siteUrl . "/$l" . $route . '/' . $page[$slugName . $suffix]);
                           $xml->endElement();
 
                       }
