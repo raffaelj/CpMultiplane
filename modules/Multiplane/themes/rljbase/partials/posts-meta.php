f8f1fedb12d906b6a754f1ac193d8d735b10eee5
Raffael <github@raffaeljesche.de>
2019-10-01 20:59:12 +0200

    added optional i18n date format for posts meta

--- modules/Multiplane/themes/rljbase/partials/posts-meta.php
+++ modules/Multiplane/themes/rljbase/partials/posts-meta.php
@@ -1,6 +1,6 @@
 
                 <p>
-                    <span class="date created">{{ date('Y-m-d H:i', $post['_created']) }}</span>
+                    <span class="date created">{{ date($app('i18n')->get('@dateformat_long', 'Y-m-d H:i'), $post['_created']) }}</span>
                     @if(!empty($post['tags'])) @foreach($post['tags'] as $tag)
                         <span class="label tag">{{ $tag }}</span>
                     @endforeach @endif
