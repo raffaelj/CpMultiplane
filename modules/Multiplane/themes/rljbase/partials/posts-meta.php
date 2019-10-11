8d15a0fc775920a1fa50934dc8638d5b52063178
Raffael <github@raffaeljesche.de>
2019-10-11 15:18:19 +0200

    added html5 time tag with datetime

--- modules/Multiplane/themes/rljbase/partials/posts-meta.php
+++ modules/Multiplane/themes/rljbase/partials/posts-meta.php
@@ -1,6 +1,6 @@
 
                 <p>
-                    <span class="date created">{{ date($app('i18n')->get('@dateformat_long', 'Y-m-d H:i'), $post['_created']) }}</span>
+                    <time class="date created" datetime="{{ date('Y-m-d H:i', $post['_created']) }}">{{ date($app('i18n')->get('@dateformat_long', 'Y-m-d H:i'), $post['_created']) }}</time>
                     @if(!empty($post['tags'])) @foreach($post['tags'] as $tag)
                         <span class="label tag">{{ $tag }}</span>
                     @endforeach @endif
