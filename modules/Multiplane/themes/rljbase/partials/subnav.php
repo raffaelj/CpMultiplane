73bb5f15b4add7c59acc078c0bfbd85cd8e639de
Raffael <github@raffaeljesche.de>
2019-10-16 17:45:59 +0200

    improved mobile multi level dropdown menu

--- modules/Multiplane/themes/rljbase/partials/subnav.php
+++ modules/Multiplane/themes/rljbase/partials/subnav.php
@@ -2,7 +2,7 @@
             <ul>
 @foreach($nav as $n)
                 <li>
-                    <a class="{{ $n['active'] ? 'active' : '' }}{{ !empty($n['children']) ? ' dropdown' : '' }}" href="@base($n[mp()->slugName])">{{ $n['title'] }}</a>@if(!empty($n['children']))<input type="checkbox" id="{{ $n['_id'] }}" /><label for="{{ $n['_id'] }}"></label>
+                    <a class="{{ $n['active'] ? 'active' : '' }}{{ !empty($n['children']) ? ' dropdown' : '' }}" href="@base($n[mp()->slugName])">{{ $n['title'] }}</a>@if(!empty($n['children']))<input type="checkbox" id="{{ $n['_id'] }}" tabindex="-1" /><label for="{{ $n['_id'] }}"></label>
                     @render('views:partials/subnav.php', ['nav' => $n['children']])
                     @endif
                 </li>
