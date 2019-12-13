d45d11cbc45fd93a6dba317dd42d2586140344dc
Raffael <github@raffaeljesche.de>
2019-12-13 12:44:51 +0100

    change classname in nav partial

--- modules/Multiplane/themes/rljbase/partials/nav.php
+++ modules/Multiplane/themes/rljbase/partials/nav.php
@@ -2,9 +2,10 @@
 $nav = mp()->getNav(null, $type);
 if (empty($nav)) return;
 $id = $type == 'main' ? ' id="nav"' : '';
+$class = $class ?? 'horizontal';
 ?>
 
-        <nav{{ $id }} class="horizontal">
+        <nav{{ $id }} class="{{ $class }}">
             @if($type == 'main')<a href="#nav" class="icon-menu"></a><a class="icon-close" href="#"></a>@endif
             @render('views:partials/subnav.php', ['nav' => $nav])
         </nav>
