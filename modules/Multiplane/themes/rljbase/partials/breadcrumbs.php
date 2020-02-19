aa99b56ad70f6076b47f8ad490a1fb2314388539
Raffael <github@raffaeljesche.de>
2020-02-19 21:49:55 +0100

    improved breadcrumbs, added home-css-icon, css cleanup

--- modules/Multiplane/themes/rljbase/partials/breadcrumbs.php
+++ modules/Multiplane/themes/rljbase/partials/breadcrumbs.php
@@ -2,18 +2,21 @@
 $breadcrumbs = mp()->breadcrumbs;
 $last = count($breadcrumbs) -1;
 if ($last == 0) return;
+// to do: display page titles instead of page slugs
 ?>
 
-<nav class="breadcrumbs">
-    <ul>
-@foreach($breadcrumbs as $k => $n)
-        <li>
-@if($k < $last)
-        <a href="@base('/'.$n)">{{ $n }}</a>
-@else
-        <span>{{ $n }}</span>
-@endif
-        </li>
-@endforeach
-    </ul>
-</nav>
+            <nav class="breadcrumbs horizontal" aria-label="@lang('Breadcrumb')">
+                <ol>
+                  @foreach($breadcrumbs as $k => $n)
+                    <li>
+                    @if($k == 0)
+                        <a href="@base('/'.$n)" title="@lang('Home')" class="icon-home"></a>
+                    @elseif($k < $last)
+                        <a href="@base('/'.$n)">{{{ $n }}}</a>
+                    @else
+                        <span>{{{ $n }}}</span>
+                    @endif
+                    </li>
+                  @endforeach
+                </ol>
+            </nav>
