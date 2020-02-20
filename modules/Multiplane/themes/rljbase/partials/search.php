97ada47abe3646f10138b981409f2752bad49feb
Raffael <github@raffaeljesche.de>
2020-02-20 22:49:13 +0100

    cleanup

--- modules/Multiplane/themes/rljbase/partials/search.php
+++ modules/Multiplane/themes/rljbase/partials/search.php
@@ -3,6 +3,6 @@
                 <form action="@base('/search')">
                     <input name="search" type="text" value="{{ $app->escape($app->param('search', '')) }}" minlength="{{ mp()->searchMinLength }}" aria-label="@lang('Search')" />
                     <input name="highlight" type="hidden" value="1" />
-                    <button type="submit"><i class="icon-search"></i>@lang('Search')</button>
+                    <button type="submit" aria-label="@lang('Search')"><i class="icon-search"></i>@lang('Search')</button>
                 </form>
             </div>
