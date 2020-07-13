30dbc6867e7dc65a65d83da64fc136962aa65844
Raffael <github@raffaeljesche.de>
2020-07-13 16:16:56 +0200

    changed url to new website

--- modules/Multiplane/themes/rljbase/partials/copyright.php
+++ modules/Multiplane/themes/rljbase/partials/copyright.php
@@ -1,2 +1,2 @@
 
-            <p class="copyright">@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy; @lang('since') 2019 <a href="https://www.raffael.one">Raffael Jesche</a></p>
+            <p class="copyright">@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy; @lang('since') 2019 <a href="https://www.rlj.me">Raffael Jesche</a></p>
