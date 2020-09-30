892aaed23551a9217b5f58b468fbf9bdba2321c2
Raffael <github@raffaeljesche.de>
2020-09-30 19:46:40 +0200

    improved posts-meta

--- modules/Multiplane/themes/rljbase/views/partials/posts-meta.php
+++ modules/Multiplane/themes/rljbase/views/partials/posts-meta.php
@@ -1,7 +1,7 @@
 
-                <p>
+                <p class="posts-meta">
                   @if(!empty($post['_created']))
-                    <time class="date created" datetime="{{ date('Y-m-d H:i', $post['_created']) }}">{{ date($app('i18n')->get('@dateformat_long', 'Y-m-d H:i'), $post['_created']) }}</time>
+                    <time class="date created" title="@lang('creation date')" datetime="{{ date('Y-m-d H:i', $post['_created']) }}">{{ date($app('i18n')->get('@dateformat_long', 'Y-m-d H:i'), $post['_created']) }}</time>
                   @endif
                   @if(!empty($post['tags']))
                     @foreach($post['tags'] as $tag)
