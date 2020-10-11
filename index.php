744227454ca35301632db931c809f83f0a8cf749
Raffael <github@raffaeljesche.de>
2020-10-11 22:01:54 +0200

    fixed empty route if $_SERVER['PATH_INFO'] is empty or incorrect

--- index.php
+++ index.php
@@ -11,5 +11,11 @@ if (PHP_SAPI == 'cli-server' && is_file(__DIR__.parse_url($_SERVER['REQUEST_URI'
 // bootstrap CpMultiplane
 require(__DIR__.'/bootstrap.php');
 
+# admin route
+if (!defined('MP_ROUTE')) {
+    $route = preg_replace('#'.preg_quote(MP_BASE_URL, '#').'#', '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), 1);
+    define('MP_ROUTE', $route == '' ? '/' : $route);
+}
+
 // run app
-$cockpit->trigger('multiplane.init')->run();
+$cockpit->set('route', MP_ROUTE)->trigger('multiplane.init')->run();
