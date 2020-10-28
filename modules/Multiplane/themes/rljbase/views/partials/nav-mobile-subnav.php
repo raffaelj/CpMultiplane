913c977fa4a72830c7d2fc8fee16f108c680598b
Raffael <github@raffaeljesche.de>
2020-10-28 11:14:26 +0100

    improved nav (markup and css)
    * allow urls if nav item has `url` key instead of `slug` key --> useful for e.g. tocs with anchor links
    * moved checkbox hack above `a` tag, so the css rule `a:focus + ul` works again
    * css cleanup --> easier to overwrite

--- modules/Multiplane/themes/rljbase/views/partials/nav-mobile-subnav.php
+++ modules/Multiplane/themes/rljbase/views/partials/nav-mobile-subnav.php
@@ -1,9 +1,11 @@
 
-              @foreach($nav as $k => $n)
-                <li class="{{ $onlyMobile ? 'nav-visible-tiny' : '' }}{{ $onlyMobile && $k == 0 ? ' nav-spacer' : ''}}">
-                    <a class="{{ $n['active'] ? 'active' : '' }}{{ !empty($n['children']) ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
-                  @if(!empty($n['children']))
-                    <input type="checkbox" id="{{ $n['_id'] }}" tabindex="-1" /><label for="{{ $n['_id'] }}"></label>
+              @foreach($nav as $k => $n){% $hasChildren = !empty($n['children']) %}
+                <li class="{{ $n['class'] ?? '' }}{{ $onlyMobile ? 'nav-visible-tiny' : '' }}{{ $onlyMobile && $k == 0 ? ' nav-spacer' : ''}}">
+                  @if($hasChildren){% $_id = uniqid('mp-nav-') %}
+                    <input type="checkbox" id="{{ $_id }}" tabindex="-1" /><label for="{{ $_id }}"></label>
+                  @endif
+                    <a class="{{ $n['active'] ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
+                  @if($hasChildren)
                     @render('views:partials/nav-subnav.php', ['nav' => $n['children']])
                   @endif
                 </li>
--- modules/Multiplane/themes/rljbase/views/partials/nav-subnav.php
+++ modules/Multiplane/themes/rljbase/views/partials/nav-subnav.php
@@ -1,10 +1,16 @@
 
-            <ul>
-              @foreach($nav as $n)
-                <li>
-                    <a class="{{ $n['active'] ? 'active' : '' }}{{ !empty($n['children']) ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
-                  @if(!empty($n['children']))
-                    <input type="checkbox" id="{{ $n['_id'] }}" tabindex="-1" /><label for="{{ $n['_id'] }}"></label>
+            <ul{{ isset($class) ? " class=\"$class\"" : '' }}>
+              @foreach($nav as $n){% $hasChildren = !empty($n['children']) %}
+                <li{{ isset($n['class']) ? " class=\"{$n['class']}\"" : '' }}>
+                  @if($hasChildren){% $_id = uniqid('mp-nav-') %}
+                    <input type="checkbox" id="{{ $_id }}" tabindex="-1" /><label for="{{ $_id }}"></label>
+                  @endif
+                  @if(isset($n[mp()->slugName]))
+                    <a class="{{ $n['active'] ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
+                  @elseif(isset($n['url']))
+                    <a class="{{ $n['active'] ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="{{ $n['url'] }}">{{{ $n['title'] }}}</a>
+                  @endif
+                  @if($hasChildren)
                     @render('views:partials/nav-subnav.php', ['nav' => $n['children']])
                   @endif
                 </li>
