9247ad6f27a9fc5821916e1f74c3bc27186b8096
Raffael <github@raffaeljesche.de>
2020-10-31 18:14:25 +0100

    prevent linebreaks in copyright partial

--- modules/Multiplane/themes/rljbase/views/partials/copyright.php
+++ modules/Multiplane/themes/rljbase/views/partials/copyright.php
@@ -1,2 +1,2 @@
 
-            <p class="copyright">@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy; @lang('since') 2019 <a href="https://www.rlj.me">Raffael Jesche</a></p>
+            <p class="copyright">@lang('built with') <a href="https://github.com/raffaelj/CpMultiplane">CpMultiplane</a> - &copy;&nbsp;@lang('since')&nbsp;2019 <a href="https://www.rlj.me">Raffael&nbsp;Jesche</a></p>
