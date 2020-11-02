101142db173a37026b85666bd6832211089eb657
Raffael <github@raffaeljesche.de>
2020-11-02 14:24:39 +0100

    cleanup

--- modules/Multiplane/themes/rljbase/views/partials/language-switch.php
+++ modules/Multiplane/themes/rljbase/views/partials/language-switch.php
@@ -4,9 +4,9 @@
                   @foreach(mp()->getLanguageSwitch($page['_id'] ?? '') as $lang)
                     <li>
                       @if($lang['active'])
-                        <span title="{{ $lang['name'] }}">{{ $lang['code'] }}</span>
+                        <span class="active" title="{{ $lang['name'] }}">{{ $lang['code'] }}</span>
                       @else
-                        <a class="{{ $lang['active'] ? 'active' : '' }}" href="{{ $lang['url'] }}" title="{{ $lang['name'] }}">{{ $lang['code'] }}</a>
+                        <a href="{{ $lang['url'] }}" title="{{ $lang['name'] }}">{{ $lang['code'] }}</a>
                       @endif
                     </li>
                   @endforeach
