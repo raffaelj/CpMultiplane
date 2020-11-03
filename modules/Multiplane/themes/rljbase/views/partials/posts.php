8877be8612691f191953b069dc048f1095017e41
Raffael <github@raffaeljesche.de>
2020-11-03 21:05:59 +0100

    fixed duplicated lang prefix in posts partial
    still only tested in new docs repo

--- modules/Multiplane/themes/rljbase/views/partials/posts.php
+++ modules/Multiplane/themes/rljbase/views/partials/posts.php
@@ -5,16 +5,19 @@ if ($path = $app->path("views:collections/{$posts['collection']['name']}/posts.p
     return;
 }
 
+$usePermalinks = mp()->usePermalinks;
+$slugName      = mp()->slugName;
+
 // make $posts, $collection and $pagination available
 extract($posts);
 ?>
 
             @render('views:partials/pagination.php', compact('pagination'))
 
-          @foreach($posts as $post)
+          @foreach($posts as $post){% $_url = $usePermalinks ? $app->routeUrl($post[$slugName]) : $app->baseUrl($pagination['posts_slug'].'/'.$post[$slugName]); %}
             <article class="excerpt">
               @if(!empty($post['title']))
-                <h3><a href="@base($pagination['posts_slug'].'/'. ($post[mp()->slugName] ?? $post['_id']))">{{{ $post['title'] }}}</a></h3>
+                <h3><a href="{{ $_url }}">{{{ $post['title'] }}}</a></h3>
               @endif
 
                 @render('views:partials/posts-meta.php', compact('post'))
@@ -27,7 +30,7 @@ extract($posts);
                 {{ $post['content'] }}
               @endif
 
-                <p class="read_more"><a href="@base($pagination['posts_slug'].'/'. ($post[mp()->slugName] ?? $post['_id']))">@lang('read more...')</a></p>
+                <p class="read_more"><a href="{{ $_url }}">@lang('read more...')</a></p>
 
             </article>
           @endforeach
