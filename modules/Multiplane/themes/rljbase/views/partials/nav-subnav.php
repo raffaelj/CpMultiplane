d8885343485dbf6c05414161130cd7b7879ca5b0
Raffael <github@raffaeljesche.de>
2020-11-08 12:04:37 +0100

    don't throw errors in nav if `active` key doesn't exist

--- modules/Multiplane/themes/rljbase/views/partials/nav-subnav.php
+++ modules/Multiplane/themes/rljbase/views/partials/nav-subnav.php
@@ -6,9 +6,9 @@
                     <input type="checkbox" id="{{ $_id }}" tabindex="-1" /><label for="{{ $_id }}"></label>
                   @endif
                   @if(isset($n[mp()->slugName]))
-                    <a class="{{ $n['active'] ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) && !mp()->usePermalinks ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
+                    <a class="{{ ($n['active'] ?? false) ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="@base(!empty($n['startpage']) && !mp()->usePermalinks ? '/' : $n[mp()->slugName])">{{{ $n['title'] }}}</a>
                   @elseif(isset($n['url']))
-                    <a class="{{ $n['active'] ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="{{ $n['url'] }}">{{{ $n['title'] }}}</a>
+                    <a class="{{ ($n['active'] ?? false) ? 'active' : '' }}{{ $hasChildren ? ' dropdown' : '' }}" href="{{ $n['url'] }}">{{{ $n['title'] }}}</a>
                   @endif
                   @if($hasChildren)
                     @render('views:partials/nav-subnav.php', ['nav' => $n['children']])
