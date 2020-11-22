c0f64a6cb0571798228f6ab2d0f07a4fedbdb4d5
Raffael <github@raffaeljesche.de>
2020-11-22 14:22:07 +0100

    use alt attribute in gallery if available

--- modules/Multiplane/themes/rljbase/views/partials/gallery.php
+++ modules/Multiplane/themes/rljbase/views/partials/gallery.php
@@ -2,7 +2,7 @@
             <div class="gallery">
               @foreach($gallery as $img)
                 <a href="@image($img['meta']['asset'])" class="thumbs" title="{{ $img['meta']['title'] ?? 'image' }}">
-                    <img src="@thumbnail($img['meta']['asset'])" alt="{{ $img['meta']['title'] ?? 'image' }}" />
+                    <img src="@thumbnail($img['meta']['asset'])" alt="{{ $img['meta']['alt'] ??  $img['meta']['title'] ?? '' }}" />
                   @if(!empty($img['meta']['title']))
                     <span>{{ $img['meta']['title'] }}</span>
                   @endif
