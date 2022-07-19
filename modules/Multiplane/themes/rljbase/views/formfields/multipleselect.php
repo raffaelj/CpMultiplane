4731cff79e7f57fcad006e51b09aa1c793cfb2a3
Raffael <github@raffaeljesche.de>
2022-07-19 19:39:20 +0200

    fixed pre-selection of sent multipleselect field after validation

--- modules/Multiplane/themes/rljbase/views/formfields/multipleselect.php
+++ modules/Multiplane/themes/rljbase/views/formfields/multipleselect.php
@@ -2,7 +2,7 @@
     @render('views:formfields/field-label.php', compact('field'))
     @render('views:formfields/field-info.php', compact('field'))
 @foreach($field['options']['options'] as $option => $label)
-{% $checked = isset($field['value']) && $field['value'] == $option ? ' checked' : ''; %}
+{% $checked = isset($field['value']) && is_array($field['value']) && in_array($option, $field['value']) ? ' checked' : ''; %}
 {% $id = $field['attr']['id'].'_'.$option; %}
 {{-- {% $required = isset($field['attr']['required']) && $field['attr']['required'] ? ' required' : ''; %}
 required doesn't work with checkboxes (without JS) --}}
