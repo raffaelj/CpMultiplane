254db65262bdb43d3b78340c5400c121f05b16f9
Raffael <github@raffaeljesche.de>
2022-07-24 17:02:14 +0200

    fixed broken honeypot

--- modules/Multiplane/themes/rljbase/views/formfields/honeypot.php
+++ modules/Multiplane/themes/rljbase/views/formfields/honeypot.php
@@ -1,9 +1,4 @@
 <?php
-$prefix = cockpit('multiplane')->formIdPrefix;
-
-$field['attr']['id']   = $prefix . $field['attr']['id'];
-$field['attr']['name'] = $prefix . $field['attr']['name'];
-
 $attributes = cockpit('multiplane')->arrayToAttributeString($field['attr']);
 ?>
 
