4c1d726fd4f4901822330fb4667b81b18fda359f
Raffael <github@raffaeljesche.de>
2022-07-25 18:39:11 +0200

    removed `value` in default search (was duplicated with advanced search)

--- modules/Multiplane/themes/rljbase/views/partials/search.php
+++ modules/Multiplane/themes/rljbase/views/partials/search.php
@@ -1,12 +1,10 @@
 <?php
-$search = $app->param('search', '');
-if (is_array($search) || strpos($search, '{') === 0) $search = '';
 if (!isset($id)) $id = 'search';
 ?>
             <div class="search">
                 <form action="@base('/search')" role="search">
                     <label for="{{ $id }}">@lang('Search in site')</label>
-                    <input name="search" type="search" value="{{{ $search }}}" minlength="{{ mp()->get('search/minLength') }}" aria-label="@lang('Search')" id="{{ $id }}" />
+                    <input name="search" type="search" minlength="{{ mp()->get('search/minLength') }}" aria-label="@lang('Search')" id="{{ $id }}" />
                     <input name="highlight" type="hidden" value="1" />
                     <button type="submit" aria-label="@lang('Search')"><i class="icon-search"></i>@lang('Search')</button>
                 </form>
