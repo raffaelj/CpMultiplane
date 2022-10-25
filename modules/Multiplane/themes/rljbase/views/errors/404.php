1402ce5facf32e8cd4b14e9b6d2752f40ab9c264
Raffael <github@raffaeljesche.de>
2022-10-25 20:07:35 +0200

    removed search form on 404 page

--- modules/Multiplane/themes/rljbase/views/errors/404.php
+++ modules/Multiplane/themes/rljbase/views/errors/404.php
@@ -3,9 +3,4 @@
             <h1>@lang('Page not found')</h1>
             <p>@lang("Something went wrong. This site doesn't exist.")</p>
             <p><a href="{{ mp()->base('/') }}">@lang('Back to start')</a></p>
-
-          @if(mp()->get('search/enabled'))
-            @render('views:partials/search.php')
-          @endif
-
         </main>
--- modules/Multiplane/themes/rljstripes/views/errors/404.php
+++ modules/Multiplane/themes/rljstripes/views/errors/404.php
@@ -4,9 +4,5 @@
                 <h1>@lang('Page not found')</h1>
                 <p>@lang("Something went wrong. This site doesn't exist.")</p>
                 <p><a href="{{ mp()->base('/') }}">@lang('Back to start')</a></p>
-
-            @if(mp()->get('search/enabled'))
-                @render('views:partials/search.php')
-            @endif
             </section>
         </main>
