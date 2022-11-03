75dc96f6d5bfb1406b2d2750cefb4410bdde770f
Raffael <github@raffaeljesche.de>
2022-11-03 16:00:15 +0100

    a11y improvements

--- modules/Multiplane/themes/rljbase/views/partials/search.php
+++ modules/Multiplane/themes/rljbase/views/partials/search.php
@@ -4,8 +4,8 @@ if (!isset($id)) $id = 'search';
             <div class="search">
                 <form action="@base('/search')" role="search">
                     <label for="{{ $id }}">@lang('Search in site')</label>
-                    <input name="search" type="search" minlength="{{ mp()->get('search/minLength') }}" aria-label="@lang('Search')" id="{{ $id }}" required />
+                    <input name="search" type="search" minlength="{{ mp()->get('search/minLength') }}" id="{{ $id }}" required />
                     <input name="highlight" type="hidden" value="1" />
-                    <button type="submit" class="icon-search" aria-label="@lang('Search')" title="@lang('Search')"></button>
+                    <button type="submit" class="icon-search" title="@lang('Search')"><span class="visually-hidden">@lang('Search')</span></button>
                 </form>
             </div>
